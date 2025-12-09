753742c67b913c816f9ab4cbb00fd864a5894de8
amanbhattsquare <aman.bhattsquare@gmail.com>
2025-12-09 10:41:42 +0530

    delete old superadmin app and install new

--- apps/superadmin/admin.py
+++ apps/superadmin/admin.py
@@ -1,14 +1,3 @@
 from django.contrib import admin
-from .models import Gym, Subscription
 
-@admin.register(Gym)
-class GymAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'is_active', 'created_at')
-    search_fields = ('name', 'email')
-    list_filter = ('is_active',)
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('gym', 'plan', 'start_date', 'end_date', 'is_active')
-    search_fields = ('gym__name', 'plan')
-    list_filter = ('is_active', 'plan')
+# Register your models here.
--- apps/superadmin/apps.py
+++ apps/superadmin/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SuperadminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.superadmin'
+    name = 'superadmin'
