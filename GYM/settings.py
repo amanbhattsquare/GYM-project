49d58efdd24e31a55df509fcf3ffb72144121360
amanbhattsquare <aman.bhattsquare@gmail.com>
2025-12-27 17:19:02 +0530

    feat: update static file settings, enhance sidebar layout, and adjust base template structure

--- GYM/settings.py
+++ GYM/settings.py
@@ -143,11 +143,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# REQUIRED for production (alwaysdata)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Media files (User uploaded files)
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
