f9bb1ac1f1c97b4e746d28e92196c88bb2909479
amanbhattsquare <aman.bhattsquare@gmail.com>
2026-01-08 17:59:13 +0530

    add paymant page in setting

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class PaymentSetting(models.Model):
+    bank_name = models.CharField(max_length=255, null=True, blank=True)
+    account_holder_name = models.CharField(max_length=255, null=True, blank=True)
+    account_number = models.CharField(max_length=255, null=True, blank=True)
+    ifsc_code = models.CharField(max_length=255, null=True, blank=True)
+    qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.bank_name or "Payment Settings"
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View
 from apps.superadmin.models import Gym
+from .models import PaymentSetting
+from .forms import PaymentSettingForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,3 +15,20 @@ class generalsetting(TemplateView):
         gym = getattr(self.request, 'gym', None)
         context['gym'] = gym
         return context
+
+class PaymentSettingView(View):
+    template_name = 'settings/payment_setting.html'
+
+    def get(self, request, *args, **kwargs):
+        payment_settings = PaymentSetting.objects.first()
+        form = PaymentSettingForm(instance=payment_settings)
+        return render(request, self.template_name, {'form': form, 'payment_settings': payment_settings})
+
+    def post(self, request, *args, **kwargs):
+        payment_settings = PaymentSetting.objects.first()
+        form = PaymentSettingForm(request.POST, request.FILES, instance=payment_settings)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Payment settings saved successfully.')
+            return redirect('settings:payment_setting')
+        return render(request, self.template_name, {'form': form, 'payment_settings': payment_settings})
