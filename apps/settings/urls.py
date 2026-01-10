b22d5cf727e34c367c76be0b1ad61bb8ee529bd9
amanbhattsquare <aman.bhattsquare@gmail.com>
2026-01-10 12:22:35 +0530

    add Gym profile page and update gym profile page

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -6,4 +6,5 @@ app_name = 'settings'
 urlpatterns = [
     path('general/', views.generalsetting.as_view(), name='general_settings'),
     path('payment/', views.PaymentSettingView.as_view(), name='payment_setting'),
+    path('profile/', views.GymProfileView.as_view(), name='gym_profile'),
 ]
